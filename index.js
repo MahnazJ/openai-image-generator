9dde1a5bc92fcedf7b8cfb1bbce7dbac3c66dcf4
Mahnaz <md.jazayeri.sev@gmail.com>
2023-07-04 14:53:41 +0200

    openai backend

---
+++ index.js
@@ -0,0 +1,13 @@
+const dotenv = require('dotenv').config();
+const express = require('express');
+const port = process.env.PORT || 3030;
+
+const app = express();
+
+//enable body parser
+app.use(express.json());
+app.use(express.urlencoded({extended: false }));
+
+app.use('/openai', require('./routes/openaiRoutes'));
+
+app.listen(port, () => console.log(`server started on port ${port}`));
