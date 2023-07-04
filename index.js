8773fa0aae63c4e1a2306973db0bc35de68c5d92
Mahnaz <md.jazayeri.sev@gmail.com>
2023-07-04 15:51:50 +0200

    added frontend

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const dotenv = require('dotenv').config();
 const express = require('express');
 const port = process.env.PORT || 3030;
@@ -7,6 +8,9 @@ const app = express();
 //enable body parser
 app.use(express.json());
 app.use(express.urlencoded({extended: false }));
+
+//set static folder
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/openai', require('./routes/openaiRoutes'));
 
